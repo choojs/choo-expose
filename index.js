1d42a5bfafcc6810df6acf6c0627f0287fae5193
jake <@o91tsjBgL0l0zs475aRWYSAXFxvirZS7VrXmirY/msI=.ed25519>
2017-07-08 23:12:12 -0400

    makes .copy(state.foo.bar) copy a specific path of the choo state object

--- index.js
+++ index.js
@@ -45,8 +45,13 @@ function expose () {
     }
 
     function copy (state) {
+      var isStateString = state && typeof state === 'string'
+      var isChooPath = isStateString && state.indexOf('state.') === 0
+
       if (!state || typeof state === 'function') state = window.choo.state
-      stateCopy(typeof state === 'string' ? pluck.apply(this, arguments) : state)
+      if (isChooPath) [].push.call(arguments, {state: window.choo.state})
+
+      stateCopy(isStateString ? pluck.apply(this, arguments) : state)
     }
   }
 }
